dbc36918843ffafbb831f704d17a7bffb93d7455
Nicholas Munson <nicholas.t.munson@gmail.com>
2018-06-24 15:19:49 -0600

    test api is attached and working

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const morgan = require("morgan")
+const data = require("./testapi.json")
 const app = express()
 
 app.use(morgan("dev"))
 
 app.get("/my-bevs", (req, res, next) => {
-    res.json()
+    res.json({data})
 })
 module.exports = app
