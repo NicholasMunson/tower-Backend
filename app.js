00559a3810268ff40a7a526483daa14bb9d967ee
Nicholas Munson <nicholas.t.munson@gmail.com>
2018-06-24 15:23:42 -0600

    changed var name from data to beers

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express")
 const morgan = require("morgan")
-const data = require("./testapi.json")
+const beers = require("./testapi.json")
 const app = express()
 
 app.use(morgan("dev"))
 
 app.get("/my-bevs", (req, res, next) => {
-    res.json({data})
+    res.json({beers})
 })
 module.exports = app
