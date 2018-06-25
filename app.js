df6db05bf1af699cbd4c212db9db2951d56f4d23
Nicholas Munson <nicholas.t.munson@gmail.com>
2018-06-24 20:30:01 -0600

    /wine and /beer both give correct information

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require("express")
 const morgan = require("morgan")
-const beers = require("./testapi.json")
+const bodyParser = require("body-parser")
 const app = express()
-const database = require("database_connection")
+const database = require("./database_connection")
 
+app.use(bodyParser.json())
 app.use(morgan("dev"))
 
-app.get("/my-bevs", (req, res, next) => {
-    res.json({beers})
+app.get("/beer", (req, res, next) => {
+    database("beer").select().then( beer => {
+        res.json({beer})
+    })
+})
+
+app.get("/wine", (req, res, next) => {
+    database("wine").select().then( wine => {
+        res.json({wine})
+    })
+})
+
+app.post("/beer", (req, res, next) =>{
+    database("beer").insert({brewery: request.body.brewery})
 })
 module.exports = app
