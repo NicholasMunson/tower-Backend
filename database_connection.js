77675284c9fb25ae36a88a1077892ad7ef742891
Nicholas Munson <nicholas.t.munson@gmail.com>
2018-06-25 01:46:10 -0600

    fixed the connection problem

--- database_connection.js
+++ database_connection.js
@@ -1,2 +1,3 @@
-const CONFIG = require("./knexfile")[process.env.NODE.ENV || "development"]
+const CONFIG = require("./knexfile")[process.env.NODE_ENV || "development"]
+
 module.exports = require("knex")(CONFIG)
--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-const app = require("./app.js")
+const app = require("./app");
+const port = parseInt(process.env.PORT || 3000);
 
-app.listen(process.env.PORT || 3000)
+app
+    .listen(port)
+    .on('error', console.error.bind(console))
+    .on('listening', console.log.bind(console, 'Listening on ' + port));
