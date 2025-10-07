6c4a232e877ae4ec4ec53b4c3c90705998ee827c
nicholasmunson <nicholas.t.munson@gmail.com>
2025-10-07 16:14:45 -0600

    updated knex file

--- knexfile.js
+++ knexfile.js
@@ -2,10 +2,30 @@ module.exports = {
   development: {
     client: "pg",
     connection: "postgres:///myBevs",
+    pool: {
+      min: 2,
+      max: 10,
+      acquireTimeoutMillis: 30000,
+      createTimeoutMillis: 30000,
+      destroyTimeoutMillis: 5000,
+      idleTimeoutMillis: 30000,
+      reapIntervalMillis: 1000,
+      createRetryIntervalMillis: 200,
+    },
   },
 
   production: {
     client: "pg",
     connection: process.env.DATABASE_URL + "?ssl=true",
+    pool: {
+      min: 2,
+      max: 20,
+      acquireTimeoutMillis: 30000,
+      createTimeoutMillis: 30000,
+      destroyTimeoutMillis: 5000,
+      idleTimeoutMillis: 30000,
+      reapIntervalMillis: 1000,
+      createRetryIntervalMillis: 200,
+    },
   },
 };
