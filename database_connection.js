ec827b75e4f153cf6acf7c08fc61aebdafe98008
nicholasmunson <nicholas.t.munson@gmail.com>
2025-10-07 17:13:58 -0600

    Refactored database_connection.js to streamline the export of the knex instance, removing connection error handling for simplicity.

--- database_connection.js
+++ database_connection.js
@@ -1,15 +1,3 @@
 const CONFIG = require("./knexfile")[process.env.NODE_ENV || "development"];
 console.log(CONFIG);
-
-// Add connection error handling
-const knex = require("knex")(CONFIG);
-
-knex
-  .raw("SELECT 1")
-  .then(() => console.log("Database connected successfully"))
-  .catch((err) => {
-    console.error("Database connection failed:", err.message);
-    process.exit(1);
-  });
-
-module.exports = knex;
+module.exports = require("knex")(CONFIG);
