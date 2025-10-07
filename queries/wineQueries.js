653c233c000cf422ca025b965859ca489c0ce133
nicholasmunson <nicholas.t.munson@gmail.com>
2025-10-07 17:08:56 -0600

    Added build script to package.json and reformatted wineQueries.js for improved readability and consistency in database operations.

--- queries/wineQueries.js
+++ queries/wineQueries.js
@@ -1,32 +1,27 @@
 const database = require("../database_connection");
 
 module.exports = {
-    list() {
-        return database("wine").select()
-    },
-    read(id) {
-        return database("wine")
-            .select()
-            .where("id", id)
-            .first()
-    },
-    create(body) {
-        return database("wine")
-            .select()
-            .insert(body)
-            .returning("*")
-            .then(record => record[0])
-    },
-    update(id, wine) {
-        return database("wine")
-            .update(wine)
-            .where("id", id)
-            .returning("*")
-            .then(record => record[0])
-    },
-    delete(id) {
-        return database("wine")
-            .delete()
-            .where("id", id)
-    }
+  list() {
+    return database("wine").select();
+  },
+  read(id) {
+    return database("wine").select().where("id", id).first();
+  },
+  create(body) {
+    return database("wine")
+      .select()
+      .insert(body)
+      .returning("*")
+      .then((record) => record[0]);
+  },
+  update(id, wine) {
+    return database("wine")
+      .update(wine)
+      .where("id", id)
+      .returning("*")
+      .then((record) => record[0]);
+  },
+  delete(id) {
+    return database("wine").where("id", id).delete();
+  },
 };
